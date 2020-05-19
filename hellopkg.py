ef7e1d28bbe4db7b40e389b7d2fce8ef38ffd1bb
Rei Alexandra Aoyama <reiaaoyama@users.noreply.github.com>
2020-05-19 07:23:32 -0700

    Create hellopkg.py
    An example of Python package

---
+++ hellopkg.py
@@ -0,0 +1,10 @@
+# An example of a Python Package
+# in this package, we have two objects
+#  HELLOMSG - a string that contain the message
+#  hello() - a function that print the message. It also returns the message
+
+HELLOMSG = "Hello world from hellopkg"
+
+func hello() :
+  print(HELLOMSG)
+  return HELLOMSG
