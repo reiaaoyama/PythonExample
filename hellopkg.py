7115e661bd70390f3604572ed35fbfe73f774f0e
Rei Alexandra Aoyama <reiaaoyama@users.noreply.github.com>
2020-05-19 07:28:05 -0700

    hellopkg.py: modify hello() to accept an argument
    This is done so that one can pass different string to the hello() function

--- hellopkg.py
+++ hellopkg.py
@@ -1,10 +1,12 @@
 # An example of a Python Package
 # in this package, we have two objects
 #  HELLOMSG - a string that contain the message
-#  hello() - a function that print the message. It also returns the message
+#  hello(@msg) - a function that print the message. It also returns the message
+#     msg is the optional argument. The default value is to print HELLOMSG
+#     but you can use a different text
 
 HELLOMSG = "Hello world from hellopkg"
 
-func hello() :
-  print(HELLOMSG)
-  return HELLOMSG
+func hello(msg = HELLOMSG):
+  print(msg)
+  return msg
